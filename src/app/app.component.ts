0fd7ccaf278f69d71a400c4b2ca4b86178f404cf
Yassine KLILICH <klilichyassine8@gmail.com>
2020-03-01 16:41:55 +0000

    implement visitMethodCall() and refactor visitPropertyRead()
    - Stoped the visitPropertyRead() from throwing errors.
    - Implemented the visitMethodCall()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,49 +7,21 @@ import { Interpolation } from './ngx-interpolation/interpolation';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent { 
-  
+
   constructor(private _interpolation: Interpolation) {
 
-    // Mixed
-    // let input: string = "I'm {{fullName.name}} from {{getCountry('Morocco')}}";
-    // let input: string = "I'm {{1 + 1}} from {{getCountry('Morocco')}}";
-    // let input: string = "I'm {{++age}} from {{getCountry('Morocco')}}";
+    ////------------------- Some Test Cases That I need to support for my library -------------------\\\\
     
-    // LiteralPrimitive
-    // let input: string = "I'm {{true}}";
-    
-    // Binary
-    // let input: string = "1 + 1 - 3";
+    // let input: string = "This is {{prop4}}"; // ==> This is Lorem ipsum
+    // let input: string = "This is {{prop1.prop2.prop3}}"; // ==> This is prop_3
+    // let input: string = "Lorem ipsum {{method_1('dolor')}} sit amet"; // ==> Lorem ipsum dolor sit amet
+    // let input: string = "Lorem ipsum {{method_1(prop4)}} sit amet"; // ==> Lorem ipsum Lorem ipsum sit amet
+    // let input: string = "Lorem ipsum {{method_1(prop1)}} sit amet"; // ==> Lorem ipsum prop_3 sit amet
+    // let input: string = "Lorem ipsum {{method_1(prop1.prop2.prop3)}} sit amet"; // ==> Lorem ipsum prop_3 sit amet
+    // let input: string = "Lorem ipsum {{prop5.method_1_of_prop5()}} sit amet"; // ==> Lorem ipsum Holla sit amet
+    // let input: string = "Lorem ipsum {{prop5.method_2_of_prop5().prop}} sit amet"; // ==> Lorem ipsum READ OBJECT'S PROPERTY RETURNED FROM A METHOD sit amet
+    let input: string = "Lorem ipsum {{prop5.method_2_of_prop5().method()}} sit amet"; // ==> Lorem ipsum EXECUTE ME sit amet
 
-    // Conditional
-    // let input: string = "{{(fullName.name == 'Yassine') ? true : false}}";
-
-    // LiteralArray
-    // let input: string = "{{[1, 2, 3]}}";
-
-    // KeyedRead
-    // let input: string = "{{fullName['name']}}";
-
-    // KeyedRead
-    // let input: string = "{{fullName['name']}}";
-
-    // LiteralMap
-    // let input: string = "{{({ top: 10 })}}";
-    
-    // SafePropertyRead
-    // let input: string = "{{fullName?.name}}";
-    
-    // SafeMethodCall
-    // let input: string = "{{fullName?.toString()}}";
-    
-    // BindingPipe
-    // let input: string = "I'm {{firstName | pipe}}";
-
-    // 
-    // let input: string = "I'm {{fullName.name}} from {{getCountry(fullName.lastName)}}";
-    // let input: string = "I'm {{fullName.toString()}} from {{getCountry(fullName.name, 'Morocco')}}";
-    let input: string = "This is {{prop1.prop2.prop3}}";
-    
     let context: any = {
       prop1: {
         prop2: {
@@ -57,13 +29,70 @@ export class AppComponent {
         },
         toString: function() { return `${this.prop2.prop3}` }
       },
+      prop4: "Lorem ipsum",
+      prop5: {
+        method_1_of_prop5: () => 'Holla',
+        method_2_of_prop5: ()=> ({prop: "READ OBJECT'S PROPERTY RETURNED FROM A METHOD", method: () => 'EXECUTE ME' })
+      },
       propNum: 10,
-      method_1: (param) => param
+      method_1: param => param,
+      method_2: (param) => {
+        return () => param
+      }
     };
 
     let result = this._interpolation.interpolate(input, context);
 
-    console.log(result);
+    // Log The Result
+    console.log(`%c${result}`, "color: #24b33b; font-size: 20px;");
   }
 
 }
+
+
+
+
+
+
+// Mixed
+// let input: string = "I'm {{fullName.name}} from {{getCountry('Morocco')}}";
+// let input: string = "I'm {{1 + 1}} from {{getCountry('Morocco')}}";
+// let input: string = "I'm {{++age}} from {{getCountry('Morocco')}}";
+
+// LiteralPrimitive
+// let input: string = "I'm {{true}}";
+
+// Binary
+// let input: string = "1 + 1 - 3";
+
+// Conditional
+// let input: string = "{{(fullName.name == 'Yassine') ? true : false}}";
+
+// LiteralArray
+// let input: string = "{{[1, 2, 3]}}";
+
+// KeyedRead
+// let input: string = "{{fullName['name']}}";
+
+// KeyedRead
+// let input: string = "{{fullName['name']}}";
+
+// LiteralMap
+// let input: string = "{{({ top: 10 })}}";
+
+// SafePropertyRead
+// let input: string = "{{fullName?.name}}";
+
+// SafeMethodCall
+// let input: string = "{{fullName?.toString()}}";
+
+// BindingPipe
+// let input: string = "I'm {{firstName | pipe}}";
+
+// FunctionCall
+// let input: string = "Lorem ipsum {{method_1('dolor')()}} sit amet";
+
+
+// 
+// let input: string = "I'm {{fullName.name}} from {{getCountry(fullName.lastName)}}";
+// let input: string = "I'm {{fullName.toString()}} from {{getCountry(fullName.name, 'Morocco')}}";
--- src/app/ngx-interpolation/interpolation-visitor.ts
+++ src/app/ngx-interpolation/interpolation-visitor.ts
@@ -17,10 +17,10 @@ import {
   SafePropertyRead,
   SafeMethodCall,
   BindingPipe,
+  FunctionCall,
 
   // Undiscovered
   Chain,
-  FunctionCall,
   PrefixNot,
   NonNullAssert,
   KeyedWrite,
@@ -29,118 +29,135 @@ import {
 } from '@angular/compiler';
 
 export class InterpolationVisitor implements AstVisitor {
-
-  private _targetAst: AST;
-  private _context: any;
-
+  
+  // Interpolation
   visitInterpolation(ast: Interpolation, context: any) {
     console.log(ast);
     return ast;
   }
 
+  // PropertyRead
   visitPropertyRead(ast: PropertyRead, context: any) {
-    if(this._targetAst == null){
-      this._targetAst = ast;
-      this._context = context;
-    }
+    let _context: any = context;
     
     if(!(ast.receiver instanceof ImplicitReceiver)) {
-      this._context = ast.receiver.visit(this, context);
-    }
-    if(ast.receiver instanceof ImplicitReceiver && this.isKeyExist(context, ast.name) === false){
-      throw `Can not find property '${ast.name}' in the context`;
-    }
-    if(!(ast.receiver instanceof ImplicitReceiver) && this.isKeyExist(this._context, ast.name) === false){
-      throw `Can not find property '${ast.name}'`;
+      _context = ast.receiver.visit(this, context);
     }
     
-    if(this._targetAst == ast) this._targetAst = null;
-    
-    return this._context[ast.name];
+    return _context[ast.name];
   }
 
+  
+  // MethodCall
   visitMethodCall(ast: MethodCall, context: any) {
+    let _context: any = context;
     let args: Array<any> = new Array();
     let argsLength: number = ast.args.length;
     
     for (let i = 0; i < argsLength; i++) {
       args.push(ast.args[i].visit(this, context));
     }
-
+    
     if(!(ast.receiver instanceof ImplicitReceiver)) {
-      ast.receiver.visit(this, context);
+      _context = ast.receiver.visit(this, context);
+    }
+    if(typeof _context[ast.name] !== 'function'){
+      throw new TypeError(`_ctx.${ast.name} is not a function`);
     }
 
-    console.log(args);
-    console.log(ast);
+    return _context[ast.name].apply(_context, args);
   }
-
+  
+  // Binary
   visitBinary(ast: Binary, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // Chain
   visitChain(ast: Chain, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // Conditional
   visitConditional(ast: Conditional, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // FunctionCall
   visitFunctionCall(ast: FunctionCall, context: any) {
-    throw new Error("Method not implemented.");
+    console.log(ast);
   }
+  
+  // ImplicitReceiver
   visitImplicitReceiver(ast: ImplicitReceiver, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // KeyedRead
   visitKeyedRead(ast: KeyedRead, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // KeyedWrite
   visitKeyedWrite(ast: KeyedWrite, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // LiteralArray
   visitLiteralArray(ast: LiteralArray, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // LiteralMap
   visitLiteralMap(ast: LiteralMap, context: any) {
     throw new Error("Method not implemented.");
   }
   
+  // LiteralPrimitive
   visitLiteralPrimitive(ast: LiteralPrimitive, context: any) {
     return ast.value;
   }
-
+  
+  // Pipe
   visitPipe(ast: BindingPipe, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // PrefixNot
   visitPrefixNot(ast: PrefixNot, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // NonNullAssert
   visitNonNullAssert(ast: NonNullAssert, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // PropertyWrite
   visitPropertyWrite(ast: PropertyWrite, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // Quote
   visitQuote(ast: Quote, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // SafeMethodCall
   visitSafeMethodCall(ast: SafeMethodCall, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // SafePropertyRead
   visitSafePropertyRead(ast: SafePropertyRead, context: any) {
     throw new Error("Method not implemented.");
   }
+  
+  // ASTWithSource
   visitASTWithSource?(ast: ASTWithSource, context: any) {
     throw new Error("Method not implemented.");
   }
   visit?(ast: AST, context?: any) {
     throw new Error("Method not implemented.");
-  }
-
-  /**
-   * Check if a key exists in an object or not.
-   * @param obj where to look for the keys.
-   * @param key to look for.
-   */
-  isKeyExist(obj: any, key: string): boolean {
-    return Object.keys(obj).includes(key);
   }
 }
